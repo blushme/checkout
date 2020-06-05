a59da90cf8a6de265d1935feae94041f5815e594
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-05 09:30:25 +0600

    https://github.com/blushme/checkout/issues/6

--- Block/ExtraSell.php
+++ Block/ExtraSell.php
@@ -12,20 +12,24 @@ class ExtraSell extends \Magento\Catalog\Block\Product\AbstractProduct {
 	 * @final Unable to use the PHP «final» keyword here because of the M2 code generation.
 	 * @override
 	 * @see \Magento\Framework\View\Element\Template::getTemplate()
+	 * @used-by \Magento\Framework\View\Element\Template::_toHtml()
+	 * @used-by \Magento\Framework\View\Element\Template::fetchView()
+	 * @used-by \Magento\Framework\View\Element\Template::getCacheKeyInfo()
+	 * @used-by \Magento\Framework\View\Element\Template::getTemplateFile()
 	 * @return string
 	 */
-	function getTemplate() {return 'BlushMe_Checkout::extra-sell.phtml';}
+	function getTemplate() {return df_asset_name('extra-sell.phtml');}
 
 	/**
 	 * 2020-06-05
 	 * @return PC
 	 */
-	function items() {
+	function items() {return dfc($this, function() {
 		$r = df_product_c(); /** @var PC $r */
 		$r->addAttributeToSelect('*');
 		$r->addAttributeToFilter('sku', ['in' => df_csv_parse(df_cfg("klarna/settings/extra_sell_products"))]);
 		$r->setVisibility([V::VISIBILITY_BOTH, V::VISIBILITY_IN_CATALOG, V::VISIBILITY_IN_SEARCH]);
 		df_stock_h()->addIsInStockFilterToCollection($r);
 		return $r;
-	}
+	});}
 }
