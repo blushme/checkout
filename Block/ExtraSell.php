3f642112a7804834c77f98259a27792733df5837
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-05 08:46:48 +0600

    https://github.com/blushme/checkout/issues/6

--- Block/ExtraSell.php
+++ Block/ExtraSell.php
@@ -1,42 +1,22 @@
 <?php
 namespace BlushMe\Checkout\Block;
+use Magento\Catalog\Model\ResourceModel\Product\Collection as PC;
+use Magento\Catalog\Model\Product\Visibility as V;
 // 2020-06-04
 // "Reimplement the «Aktuella erbjudanden» block": https://github.com/blushme/checkout/issues/6
 // I have ported the code from here:
 // https://github.com/blushme/site/blob/2020-04-28-before-Klarna-upgrade/app/code/Seavus/Klarna/Block/Checkout/Extrasell.php
 class ExtraSell extends \Magento\Catalog\Block\Product\AbstractProduct {
-	function __construct(
-		\Magento\Catalog\Block\Product\Context $context,
-		\Magento\Catalog\Model\ProductRepository $productRepository,
-		\Magento\Framework\App\ResourceConnection $resourceConnection,
-		array $data = []
-	) {
-		$this->productRepository = $productRepository;
-		$this->resourceConnection = $resourceConnection;
-		parent::__construct($context, $data);			
+	/**
+	 * 2020-06-05
+	 * @return PC
+	 */
+	function items() {
+		$r = df_product_c(); /** @var PC $r */
+		$r->addAttributeToSelect('*');
+		$r->addAttributeToFilter('sku', ['in' => df_csv_parse(df_cfg("klarna/settings/extra_sell_products"))]);
+		$r->setVisibility([V::VISIBILITY_BOTH, V::VISIBILITY_IN_CATALOG, V::VISIBILITY_IN_SEARCH]);
+		df_stock_h()->addIsInStockFilterToCollection($r);
+		return $r;
 	}
-	
-	function getExtraSellProducts(){
-		$products = array();
-		$extra_sell_products = df_cfg("klarna/settings/extra_sell_products");
-		if($extra_sell_products != ""){
-			$extra_sell_products = explode(",", $extra_sell_products);
-			if(!empty($extra_sell_products)){
-				foreach($extra_sell_products as $extra_sell_product){
-					$connection = $this->resourceConnection->getConnection();
-					$result = $connection->fetchOne('SELECT entity_id FROM catalog_product_entity WHERE sku = "' . $extra_sell_product . '"');
-					if($result){
-						$reloaded_product = $this->productRepository->get($extra_sell_product);
-						if($reloaded_product->isSaleable() && $reloaded_product->getVisibility() != "1"){
-							$products[] = $reloaded_product;
-						}
-					}
-				}
-			}
-		}
-		return $products;
-	}
-
-	protected $productRepository;
-	protected $resourceConnection;
 }
