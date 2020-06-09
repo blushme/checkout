bd061f2eb29d97ce7296de309d33b619f61d72f8
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-09 14:36:24 +0600

    https://github.com/blushme/checkout/issues/7

--- view/frontend/web/main.js
+++ view/frontend/web/main.js
@@ -3,7 +3,16 @@ define([], function() {return function() {require(['jquery', 'domReady!'], funct
 	$(document).on('click', '.more, .less', function() {
 		var $this = $(this);
 		var $siblings;
-		if ($this.closest('#minicart-content-wrapper').length) {
+		// 2020-06-09 I check `location` just for performance.
+		if (0 === location.pathname.indexOf('/checkout/') && $this.closest('#shopping-cart-table').length) {
+			// 2020-06-09
+			// "Handle the `+` / `-` buttons in the cart block of the checkout page":
+			// https://github.com/blushme/checkout/issues/7
+			$siblings = $this.siblings('.input-text');
+			$siblings.val(parseInt($siblings.data('value')) + ($this.hasClass('more') ? 1 : -1));
+			$('#button-' + $siblings.attr('id')).click();
+		}
+		else if ($this.closest('#minicart-content-wrapper').length) {
 			// 2020-06-08
 			// "The `+` and `-` buttons initialization
 			// by the `app/code/Seavus/Categories/view/frontend/templates/sidebar/content.phtml` file
@@ -18,14 +27,6 @@ define([], function() {return function() {require(['jquery', 'domReady!'], funct
 			$siblings.val(newAdd);
 			$siblings.attr('data-item-qty', newAdd);
 			$('#update-cart-item-' + $this.siblings('.update-cart-item').attr('data-cart-item')).click();
-		}
-		else if ($this.closest('#shopping-cart-table').length) {
-			// 2020-06-09
-			// "Handle the `+` / `-` buttons in the cart block of the checkout page":
-			// https://github.com/blushme/checkout/issues/7
-			$siblings = $this.siblings('.input-text');
-			$siblings.val(parseInt($siblings.data('value')) + ($this.hasClass('more') ? 1 : -1));
-			$('#button-' + $siblings.attr('id')).click();
 		}
 	});
 });}});
